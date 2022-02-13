cbf784296c7b7e1545818f6371bfa34753874c84
RobertCarrUTA <robertcarrcisd@gmail.com>
2022-02-13 16:08:44 -0600

    Added some minor color changes and fixes to the homepage to match the color scheme of UTA blue, orange, and white. Renamed app.
    Also added a very basic review page (that is accessible from the homepage button) with a scrollview that will eventually allow the user to scroll through reviews and replies once we populate it with those features.

--- app/src/main/java/com/example/cse3311project/MainActivity.java
+++ app/src/main/java/com/example/cse3311project/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.cse3311project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button SearchButton_Homepage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* We will need a search bar eventually,
+        official documentation for that can be found here:
+        https://developer.android.com/guide/topics/search/search-dialog */
+
+
+        SearchButton_Homepage = (Button) findViewById(R.id.SearchButton_Homepage);
+        SearchButton_Homepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+
+    // Use this function to open any new activity as seen above
+    public void openNewActivity()
+    {
+        Intent intent = new Intent(this, ReviewActivity.class);
+        startActivity(intent);
     }
 }
