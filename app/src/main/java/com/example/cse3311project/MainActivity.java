a9b188b2f3472c5ee86009711fa8695976b4946d
RobertCarrUTA <robertcarrcisd@gmail.com>
2022-02-13 21:48:51 -0600

    Added some minor changes, put in a default user avatar on the homepage that needs to be changed. I made it myself in Adobe Illustrator.

--- app/src/main/java/com/example/cse3311project/MainActivity.java
+++ app/src/main/java/com/example/cse3311project/MainActivity.java
@@ -7,32 +7,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     private Button SearchButton_Homepage, accountButton_Homepage;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* We will need a search bar eventually,
+        /*
+        Robert is putting these notes here for later use, ignore if you want.
+
+        For implementing user picked avatars, maybe look here:
+        https://stackoverflow.com/questions/38352148/get-image-from-the-gallery-and-show-in-imageview
+
+        We will need a search bar eventually,
         official documentation for that can be found here:
-        https://developer.android.com/guide/topics/search/search-dialog */
+        https://developer.android.com/guide/topics/search/search-dialog
+        */
 
-
+        // Identifying our buttons that are located on the .xml file
         SearchButton_Homepage = (Button) findViewById(R.id.SearchButton_Homepage);
         accountButton_Homepage = (Button) findViewById(R.id.accountButton_Homepage);
 
 
-        accountButton_Homepage.setOnClickListener(new View.OnClickListener() {
+        // On click listeners that allow the user to go from page to page
+        // when clicking on a button
+        accountButton_Homepage.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View v) {
                 openAccountActivity();
             }
         });
 
-        SearchButton_Homepage.setOnClickListener(new View.OnClickListener() {
+        SearchButton_Homepage.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View v) {
                 openReviewActivity();
