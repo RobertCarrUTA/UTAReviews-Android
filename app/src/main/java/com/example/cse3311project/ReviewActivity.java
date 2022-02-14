67165afcd606fe9799bf66842e2d6c2c8596ed43
RobertCarrUTA <robertcarrcisd@gmail.com>
2022-02-13 21:48:51 -0600

    Added some minor changes, put in a default user avatar on the homepage that needs to be changed. I made it myself in Adobe Illustrator.

--- app/src/main/java/com/example/cse3311project/ReviewActivity.java
+++ app/src/main/java/com/example/cse3311project/ReviewActivity.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ReviewActivity extends AppCompatActivity {
-
+public class ReviewActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
     }
