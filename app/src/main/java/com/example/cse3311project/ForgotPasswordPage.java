e3be338251ac43d3216396b7d333cd3508d2dec5
RobertCarrUTA <robertcarrcisd@gmail.com>
2022-02-14 20:43:31 -0600

    Code cleanup.

--- app/src/main/java/com/example/cse3311project/ForgotPasswordPage.java
+++ app/src/main/java/com/example/cse3311project/ForgotPasswordPage.java
@@ -16,7 +16,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class ForgotPasswordPage extends AppCompatActivity {
+public class ForgotPasswordPage extends AppCompatActivity
+{
     EditText Email;
     Button ResetPassword;
 
@@ -27,8 +28,10 @@ public class ForgotPasswordPage extends AppCompatActivity {
         setContentView(R.layout.activity_forgot_password_page);
 
         Email = findViewById(R.id.RecoveringEmail);
-        ResetPassword.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        ResetPassword.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
                 Toast.makeText(ForgotPasswordPage.this, "Password Reset sent to Email", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(getApplicationContext(), LoginPage.class));
             }
