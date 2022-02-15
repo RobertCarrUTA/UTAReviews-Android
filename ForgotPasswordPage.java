70fc3b449ee417ad3b1bf02b0da7e1f26576242e
khado123 <82847167+khado123@users.noreply.github.com>
2022-02-14 19:03:49 -0600

    Add files via upload

---
+++ ForgotPasswordPage.java
@@ -0,0 +1,36 @@
+package com.example.utareviewing;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognitionService;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class ForgotPasswordPage extends AppCompatActivity {
+    EditText Email;
+    Button ResetPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        Email = findViewById(R.id.RecoveringEmail);
+        ResetPassword.setOnClickListene(new View.OnClickListener(){
+            public void onClick(View v){
+                Toast.makeText(ForgotPasswordPage.this, "Password Reset sent to Email", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(),LoginPage.class));
+            }
+        });
+    }
+}
