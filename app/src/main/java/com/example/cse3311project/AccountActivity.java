59561559ddcf94f251004342943b1dab88edde3e
RobertCarrUTA <robertcarrcisd@gmail.com>
2022-03-06 17:29:23 -0600

    Added a default username to all users based on their emails. Also made dividers in the search bar. The account page now shows users their current email address and default usernames.

--- app/src/main/java/com/example/cse3311project/AccountActivity.java
+++ app/src/main/java/com/example/cse3311project/AccountActivity.java
@@ -6,10 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 
 public class AccountActivity extends AppCompatActivity
 {
     private Button returnHomeButton_AccountPage;
+    private EditText editTextTextEmailAddress, editTextTextUsername;
+
+    FirebaseAuth fAuth;
+
+    private DatabaseReference ProfessorDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,7 +28,23 @@ public class AccountActivity extends AppCompatActivity
         setContentView(R.layout.activity_account);
 
         returnHomeButton_AccountPage = (Button) findViewById(R.id.returnHomeButton_AccountPage);
+        editTextTextEmailAddress = (EditText)  findViewById(R.id.editTextTextEmailAddress);
+        editTextTextUsername = (EditText) findViewById(R.id.editTextTextUsername);
 
+        editTextTextEmailAddress.setEnabled(false);
+        editTextTextUsername.setEnabled(false);
+
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentUser != null)
+        {
+            String userEmail = currentUser.getEmail();
+            editTextTextEmailAddress.setText(userEmail);
+
+            //String username = editTextTextEmailAddress.toString();
+            String[] splits = userEmail.split("@");
+            editTextTextUsername.setText("@" + splits[0]);
+        }
 
         returnHomeButton_AccountPage.setOnClickListener(new View.OnClickListener()
         {
