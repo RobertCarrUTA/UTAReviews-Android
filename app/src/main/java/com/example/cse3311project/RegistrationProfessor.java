1c30409fe3d881a3a8ba39389d073238c5dabba0
RobertCarrUTA <robertcarrcisd@gmail.com>
2022-03-08 01:41:24 -0600

    Spinner on registration page for student or professor is now included. Have merged with Zak's additions and made small changes.

---
+++ app/src/main/java/com/example/cse3311project/RegistrationProfessor.java
@@ -0,0 +1,14 @@
+package com.example.cse3311project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationProfessor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_professor);
+    }
+}
