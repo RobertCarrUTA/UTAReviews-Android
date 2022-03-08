3b76d32c869d155d4dd8f33ab575dbafdd48aacc
RobertCarrUTA <robertcarrcisd@gmail.com>
2022-03-07 20:37:09 -0600

    Reviews can now be taken from the database.

--- app/src/main/java/com/example/cse3311project/MainActivity.java
+++ app/src/main/java/com/example/cse3311project/MainActivity.java
@@ -36,7 +36,6 @@ public class MainActivity extends AppCompatActivity
     private EditText professorSearchBar;
 
     FirebaseAuth fAuth;
-
     private DatabaseReference ProfessorDatabase;
 
     @Override
