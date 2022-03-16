fbd4066a8efcf209d736599abcd3ddbb35102fe5
RobertCarrUTA <robertcarrcisd@gmail.com>
2022-03-16 01:16:44 -0500

    Reviews working, I have also went into all of our Java files and made the code better and removed all warnings in our code that Android Studio was throwing warnings about, but only in the Java files.

--- app/src/main/java/com/example/cse3311project/Registration.java
+++ app/src/main/java/com/example/cse3311project/Registration.java
@@ -1,8 +1,5 @@
 package com.example.cse3311project;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -14,13 +11,14 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class Registration extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
@@ -70,76 +68,63 @@ public class Registration extends AppCompatActivity implements AdapterView.OnIte
         }
 
 
-        submitButton.setOnClickListener(new View.OnClickListener()
+        submitButton.setOnClickListener(v ->
         {
-            @Override
-            public void onClick(View v)
+            databaseStart = FirebaseDatabase.getInstance();
+            database = databaseStart.getReference( "users");
+            String password = idInput.getText().toString().trim();
+            String email = emailInput.getText().toString().trim();
+            String accountType = spinner.getSelectedItem().toString();
+            String[] emailSplit = email.split("@");
+
+            if (TextUtils.isEmpty(password))
             {
-                databaseStart = FirebaseDatabase.getInstance();
-                database = databaseStart.getReference( "users");
-                String password = idInput.getText().toString().trim();
-                String email = emailInput.getText().toString().trim();
-                String accountType = spinner.getSelectedItem().toString();
-                String[] emailSplit = email.split("@");
+                idInput.setError("A password is required");
+            }
 
-                if (TextUtils.isEmpty(password))
-                {
-                    idInput.setError("A password is required");
-                }
+            if (password.length() < 8)
+            {
+                idInput.setError("Password must be 8 characters or greater");
+            }
 
-                if (password.length() < 8)
+            if (TextUtils.isEmpty(email))
+            {
+                emailInput.setError("Email is Required.");
+                //return;
+            }
+            else if (!email.contains("@"))
+            {
+                emailInput.setError("Please enter a valid email address");
+                //return;
+            }
+            else if (!(emailSplit[1].equals("mavs.uta.edu") || emailSplit[1].equals("uta.edu")))
+            {
+                emailInput.setError("Must be a UTA email");
+            }
+            else
+            {
+                // Registration of the user in the database. If the user is successfully registered, show a toast and redirect to login page
+                // if a user is not successfully registered, show the error that occurred
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task ->
                 {
-                    idInput.setError("Password must be 8 characters or greater");
-                }
-
-                if (TextUtils.isEmpty(email))
-                {
-                    emailInput.setError("Email is Required.");
-                    //return;
-                }
-                else if (!email.contains("@"))
-                {
-                    emailInput.setError("Please enter a valid email address");
-                    //return;
-                }
-                else if (!(emailSplit[1].equals("mavs.uta.edu") || emailSplit[1].equals("uta.edu")))
-                {
-                    emailInput.setError("Must be a UTA email");
-                }
-                else
-                {
-                    // Registration of the user in the database. If the user is successfully registered, show a toast and redirect to login page
-                    // if a user is not successfully registered, show the error that occurred
-                    fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                    if (task.isSuccessful())
                     {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task)
-                        {
-                            if (task.isSuccessful())
-                            {
-                                FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
-                                database.child(accountType).push().setValue(currentFirebaseUser.getUid());
-                                Toast.makeText(Registration.this, "Account Created Successfully", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(getApplicationContext(), LoginPage.class));
-                            }
-                            else
-                            {
-                                Toast.makeText(Registration.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    });
-                }
+                        FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
+                        assert currentFirebaseUser != null;
+                        database.child(accountType).push().setValue(currentFirebaseUser.getUid());
+                        Toast.makeText(Registration.this, "Account Created Successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(), LoginPage.class));
+                    }
+                    else
+                    {
+                        Toast.makeText(Registration.this, "Error: "
+                                + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
 
-        cancelButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                startActivity(new Intent(getApplicationContext(), LoginPage.class));
-            }
-        });
+        cancelButton.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), LoginPage.class)));
     }
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int i, long l)
