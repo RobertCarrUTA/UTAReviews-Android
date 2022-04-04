ca8aa188beb86010335ddb8e398d46dfefaf032e
khado123 <82847167+khado123@users.noreply.github.com>
2022-04-03 22:25:06 -0500

    Add files via upload

--- ForgotPasswordPage.java
+++ ForgotPasswordPage.java
@@ -3,16 +3,12 @@ package com.example.cse3311project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 
 
@@ -28,47 +24,51 @@ public class ForgotPasswordPage extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password_page);
 
-        Email = findViewById(R.id.Email);
+        //variable declaration
+        Email = findViewById(R.id.RecoveringEmail);
         ResetPassword = findViewById(R.id.buttonSendingCode);
         auth = FirebaseAuth.getInstance();
 
-        ResetPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = Email.getText().toString().trim();
-                String[] emailSplit = email.split("@");
-                if (TextUtils.isEmpty(email)) {
-                    Email.setError("Email is Required.");
-                    return;
-                }
-
-                if (!email.contains("@")) {
-                    Email.setError("Please enter an UTA email");
-                    return;
-                }
-
-                if (emailSplit[1].equals("mavs.uta.edu") || emailSplit[1].equals("uta.edu")) {
-                }
-                else
-                {
-                    Email.setError("Must be UTA email");
-                    return;
-                }
-                auth.sendPasswordResetEmail(email).addOnSuccessListener(new OnSuccessListener<Void>(){
-
-                    @Override
-                    public void onSuccess(@NonNull Void unused) {
-                        Toast.makeText(ForgotPasswordPage.this, "Password Reset sent to Email", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(getApplicationContext(), LoginPage.class));
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(ForgotPasswordPage.this, "This email has not been registered", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(getApplicationContext(), LoginPage.class));
-                    }
-                });
+        // Verifiled if the email is valid when the user hits Reset button
+        //This also checks if the email is uta outlook email
+        ResetPassword.setOnClickListener(v ->
+        {
+            String email = Email.getText().toString().trim();
+            String[] emailSplit = email.split("@");
+            if (TextUtils.isEmpty(email))
+            {
+                Email.setError("Email is Required.");
+                return;
             }
+
+            if (!email.contains("@"))
+            {
+                Email.setError("Please enter a valid email");
+                return;
+            }
+
+            if (emailSplit[1].equals("mavs.uta.edu") || emailSplit[1].equals("uta.edu"))
+            {
+                Email.setError("Must be a UTA email");
+            }
+            else
+            {
+                Email.setError("Must be UTA email");
+                return;
+            }
+
+
+
+            //Output the result to the user
+            auth.sendPasswordResetEmail(email).addOnSuccessListener(unused ->
+            {
+                Toast.makeText(ForgotPasswordPage.this, "Password Reset sent to Email", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), LoginPage.class));
+            }).addOnFailureListener(e ->
+            {
+                Toast.makeText(ForgotPasswordPage.this, "This email has not been registered", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), LoginPage.class));
+            });
         });
     }
 }
