20ae0265bf9393197c84f9b4c41b90b2d23cc94f
khado123 <82847167+khado123@users.noreply.github.com>
2022-04-03 23:44:45 -0500

    Add files via upload

--- LoginPage.java
+++ LoginPage.java
@@ -1,30 +1,37 @@
 package com.example.cse3311project;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class LoginPage extends AppCompatActivity
 {
     EditText Email, Password;
-    Button Login, signupButton, forgotPasswordButton;
+    Button Login;
+    TextView signupButton, forgotPasswordButton;
     FirebaseAuth auth;
 
 
     protected void onCreate(Bundle savedInstanceSate)
     {
+        // Variable declaration and import database
         super.onCreate(savedInstanceSate);
         setContentView(R.layout.activity_login_page);
         Email = findViewById(R.id.EmailAddress);
@@ -34,76 +41,62 @@ public class LoginPage extends AppCompatActivity
         forgotPasswordButton = findViewById(R.id.forgotPasswordButton);
         auth = FirebaseAuth.getInstance();
 
-        Login.setOnClickListener(new View.OnClickListener()
+        //If the Login button Click then the function will check if the email is valid an dit is registered
+        // then it will check the password if it matched the on in the system
+        Login.setOnClickListener(v ->
         {
-            @Override
-            public void onClick (View v)
+            String email = Email.getText().toString().trim();
+            String password = Password.getText().toString().trim();
+            String[] emailSplit = email.split("@");
+            if(TextUtils.isEmpty(email))
             {
-                String email = Email.getText().toString().trim();
-                String password = Password.getText().toString().trim();
-                String[] emailSplit = email.split("@");
-                if(TextUtils.isEmpty(email)) {
-                    Email.setError("Email is Required.");
-                    return;
-                }
+                Email.setError("Email is Required.");
+                return;
+            }
 
-                if(!email.contains("@"))
-                {
-                    Email.setError("Please enter an UTA email");
-                    return;
-                }
 
-                if(emailSplit[1].equals("mavs.uta.edu") || emailSplit[1].equals("uta.edu"))
+            if(!email.contains("@"))
+            {
+                Email.setError("Please enter a UTA email");
+                return;
+            }
+
+            else if (!(emailSplit[1].equals("mavs.uta.edu") || emailSplit[1].equals("uta.edu")))
+            {
+                Email.setError("Must be UTA email");
+            }
+
+
+            if (TextUtils.isEmpty(password))
+            {
+                Password.setError("Password is Required.");
+                return;
+            }
+
+            if (password.length() < 8)
+            {
+                Password.setError("Password must have 8 characters or more.");
+                return;
+            }
+
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task ->
+            {
+                if(task.isSuccessful())
                 {
+                    Toast.makeText(LoginPage.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 }
                 else
                 {
-                    Email.setError("Must be UTA email");
-                    return;
+                    Toast.makeText(LoginPage.this, "Wrong Email or Password"
+                            + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
                 }
-                if (TextUtils.isEmpty(password)) {
-                    Password.setError("Password is Required.");
-                    return;
-                }
-                if (password.length() < 8) {
-                    Password.setError("Password must have 8 characters or more.");
-                    return;
-                }
-                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful())
-                        {
-                            Toast.makeText(LoginPage.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        }
-                        else
-                        {
-                            Toast.makeText(LoginPage.this, "Wrong Email or Password" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-            }
-
+            });
         });
 
-        forgotPasswordButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                startActivity(new Intent(getApplicationContext(), ForgotPasswordPage.class));
-            }
-        });
+        forgotPasswordButton.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), ForgotPasswordPage.class)));
 
-        signupButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                startActivity(new Intent(getApplicationContext(), Registration.class));
-            }
-        });
+        signupButton.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), Registration.class)));
 
     }
 }
---
+++ Registration.java
@@ -0,0 +1,167 @@
+package com.example.cse3311project;
+
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
+
+public class Registration extends AppCompatActivity implements AdapterView.OnItemSelectedListener
+{
+    EditText lastNameInput, emailInput, idInput;
+    Button submitButton, cancelButton;
+    Spinner spinner;
+    FirebaseAuth fAuth;
+    private FirebaseDatabase databaseStart;
+    private DatabaseReference database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        //variable declare
+        lastNameInput = findViewById(R.id.lastNameInput);
+        emailInput = findViewById(R.id.emailInput);
+        idInput = findViewById(R.id.idInput);
+
+
+        submitButton = findViewById(R.id.submitButton);
+        cancelButton = findViewById(R.id.cancelButton);
+
+        spinner = findViewById(R.id.registrationTypeInput);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
+        (
+            this, R.array.registrationTypeArray, android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource
+        (
+            android.R.layout.simple_spinner_dropdown_item
+        );
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        // If a user is already signed in, they should not be on the registration page
+        // They should instead be redirected to the homepage
+        if(fAuth.getCurrentUser() != null)
+        {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+
+
+        // if the submit button is clicked
+        //the function will check if the requirements matched with the database
+        submitButton.setOnClickListener(v ->
+        {
+            databaseStart = FirebaseDatabase.getInstance();
+            database = databaseStart.getReference( "users");
+            String password = idInput.getText().toString().trim();
+            String email = emailInput.getText().toString().trim();
+            String accountType = spinner.getSelectedItem().toString();
+            String[] emailSplit = email.split("@");
+
+            if (TextUtils.isEmpty(password))
+            {
+                idInput.setError("A password is required");
+            }
+
+            if (password.length() < 8)
+            {
+                idInput.setError("Password must be 8 characters or greater");
+            }
+
+            if (TextUtils.isEmpty(email))
+            {
+                emailInput.setError("Email is Required.");
+                //return;
+            }
+
+            else if (!email.contains("@"))
+            {
+                emailInput.setError("Please enter a valid email address");
+                //return;
+            }
+            else if (!(emailSplit[1].equals("mavs.uta.edu") || emailSplit[1].equals("uta.edu")))
+            {
+                emailInput.setError("Must be a UTA email");
+            }
+
+
+            else
+            {
+                // Registration of the user in the database. If the user is successfully registered, show a toast and redirect to login page
+                // if a user is not successfully registered, show the error that occurred
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task ->
+                {
+                    if (task.isSuccessful())
+                    {
+                        //Email verification
+
+                        FirebaseUser user = fAuth.getCurrentUser();
+                        user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Registration.this, "Verification Email has been sent.", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d(TAG,"onFailure: Email not sent "+e.getMessage());
+                            }
+                        });
+
+                        FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
+                        assert currentFirebaseUser != null;
+                        database.child(accountType).push().setValue(currentFirebaseUser.getUid());
+                        Toast.makeText(Registration.this, "Account Created Successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(), LoginPage.class));
+                    }
+                    else
+                    {
+                        Toast.makeText(Registration.this, "Error: "
+                                + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
+        cancelButton.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), LoginPage.class)));
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int i, long l)
+    {
+        //Toast.makeText(parent.getContext(),"Selection was made",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView)
+    {
+
+    }
+}
