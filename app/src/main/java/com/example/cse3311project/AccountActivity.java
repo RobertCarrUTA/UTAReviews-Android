af8b43abf7f8e4f58199b2d69ef343f466f3833b
RobertCarrUTA <robertcarrcisd@gmail.com>
2022-04-04 20:34:33 -0500

    Merged me and Kha's branches. Users can now change their username just on the account page, it does not affect Firebase information, it will need to be worked on to where the username actually gets saved to that user.

--- app/src/main/java/com/example/cse3311project/AccountActivity.java
+++ app/src/main/java/com/example/cse3311project/AccountActivity.java
@@ -2,51 +2,101 @@ package com.example.cse3311project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class AccountActivity extends AppCompatActivity
+public class AccountActivity extends AppCompatActivity implements UsernameDialog.ExampleDialogListener
 {
-
+    Button resendCode;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
+    private TextView editTextTextUsername;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // Documentation for setting up a username?:
+        // https://firebase.google.com/docs/auth/android/manage-users#update_a_users_profile
+
+        //Declaration for variable
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
-        Button returnHomeButton_AccountPage = findViewById(R.id.returnHomeButton_AccountPage);
-        EditText editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
-        EditText editTextTextUsername = findViewById(R.id.editTextTextUsername);
+        Button returnHomeButton_AccountPage = (Button) findViewById(R.id.returnHomeButton_AccountPage);
+        TextView editTextTextEmailAddress = (TextView) findViewById(R.id.editTextTextEmailAddress);
+        editTextTextUsername = (TextView) findViewById(R.id.editTextTextUsername);
+        Button Credentials = (Button) findViewById(R.id.ChangingAccountCredentials);
+        Button Avatar = (Button) findViewById(R.id.changeAvatarButton_AccountPage);
 
         editTextTextEmailAddress.setEnabled(false);
         editTextTextUsername.setEnabled(false);
 
-
+        // Import Database
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        //declare variables for resending verified Code for the email
+        resendCode = findViewById(R.id.resendCode);
+        userID = fAuth.getCurrentUser().getEmail();
+        FirebaseUser user = fAuth.getCurrentUser();
+
+        // this if statement is verified the email is not empty and it is an email address
         if(currentUser != null)
         {
             String userEmail = currentUser.getEmail();
             editTextTextEmailAddress.setText(userEmail);
 
             //String username = editTextTextEmailAddress.toString();
+            /*
             assert userEmail != null;
             String[] splits = userEmail.split("@");
             String usernameString = "@" + splits[0];
             editTextTextUsername.setText(usernameString);
+             */
         }
 
+        Credentials.setOnClickListener(view -> showDialog());
+
+        if(!user.isEmailVerified())
+        {
+            //setting Verify Now invisible for email that is not verified
+            resendCode.setVisibility(View.VISIBLE);
+            resendCode.setOnClickListener(view -> user.sendEmailVerification().addOnSuccessListener(
+                    aVoid -> Toast.makeText(view.getContext(), "Verification Email has been sent.",
+                            Toast.LENGTH_SHORT).show()).addOnFailureListener(e ->
+                                Log.d("tag","onFailure: Email not sent "+e.getMessage())));
+        }
         returnHomeButton_AccountPage.setOnClickListener(v -> openHomeActivity());
     }
 
+    // this function calls the dialog and merge it into the accountactivity
+    public void showDialog()
+    {
+        UsernameDialog usernameDialog = new UsernameDialog();
+        usernameDialog.show(getSupportFragmentManager(),"Change Username");
+    }
+
     // Use this function to open the home (Main) activity
+
     public void openHomeActivity()
     {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void applyTexts(String username)
+    {
+        editTextTextUsername.setText(username);
     }
 }
---
+++ app/src/main/java/com/example/cse3311project/UsernameDialog.java
@@ -0,0 +1,68 @@
+package com.example.cse3311project;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class UsernameDialog extends AppCompatDialogFragment
+{
+    private EditText editTextUsername;
+    private ExampleDialogListener listener;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState)
+    {
+        // building a dialog from the xml file named username_dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_username_dialog, null);
+
+        // Documentation for setting up a username?:
+        // https://firebase.google.com/docs/auth/android/manage-users#update_a_users_profile
+        //setting up the title and the buttons cancel and ok
+        builder.setView(view);
+        builder.setTitle("Change Username");
+        builder.setNegativeButton("Cancel", (dialogInterface, i) ->
+        {
+
+        }).setPositiveButton("Ok", (dialogInterface, i) -> {
+            // declare the string to get new username and pass it to applyTexts
+            String username = editTextUsername.getText().toString();
+            listener.applyTexts(username);
+        });
+
+        editTextUsername = view.findViewById(R.id.new_username);
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context)
+    {
+        super.onAttach(context);
+        try
+        {
+            listener = (ExampleDialogListener) context;
+        }
+        catch (ClassCastException e)
+        {
+            throw new ClassCastException(context.toString() + "must implement ExampleDialogListener");
+        }
+    }
+
+    // this function is passing the new username to account activity.
+    public interface ExampleDialogListener
+    {
+        void applyTexts(String username);
+    }
+}
